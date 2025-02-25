5687f4c6f93e2e36d03249c52fab6d326a3941db
Asadullah <129300889+Asadullah-byte@users.noreply.github.com>
2025-02-25 11:03:53 +0500

    changed func: googleOAuth --> googleAuth

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv, { config } from "dotenv";
 import cors from "cors";
 import authRoutes from "./src/routes/authRoutes.js";
 import { connectDB } from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,13 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use(express.json()); //allows us to parse incoming requests with JSON payloads
+app.use(cookieParser());
+app.use(cors({origin:"http://localhost:5173", credentials: true}));
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
 
 app.use("/api/auth", authRoutes);
 
