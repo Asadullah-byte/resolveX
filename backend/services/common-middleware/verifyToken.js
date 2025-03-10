523d4533f3962e5a3fa0acc2618b3e26a022d8a0
Asadullah <129300889+Asadullah-byte@users.noreply.github.com>
2025-03-10 14:58:43 +0500

    UPDATED: made verification token available to everyone

---
+++ backend/services/common-middleware/verifyToken.js
@@ -0,0 +1,44 @@
+import JsonWebToken from "jsonwebtoken";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const publicKey = fs.readFileSync(
+  path.join(__dirname, "../auth-service/src/keys/public.pem"),
+  "utf-8"
+);
+
+
+
+export const verifyToken = (req, res, next) => {
+  const jwt = req.cookies.jwt;
+  if (!jwt)
+    return res
+      .status(401)
+      .json({ success: false, message: "Unauthorized - no cookie issued" });
+
+  try {
+    const decoded = JsonWebToken.verify(jwt,publicKey,{algorithms: ["RS256"]});
+
+    if (!decoded) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Unauthorized -  invalid token" });
+    }
+    req.user = { id: decoded.userId };
+    next();
+  } catch (error) {
+    console.error("Error in verifyToken:", error);
+  
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ success: false, message: "Unauthorized - Invalid token" });
+    } else if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ success: false, message: "Unauthorized - Token expired" });
+    }
+  
+    return res.status(500).json({ success: false, message: "Internal server error" });
+  }
+  
+};
